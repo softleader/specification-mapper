fb145a1ef7829b251518c736026e24659a676ee4
Shihyu Ho <methodho@gmail.com>
2022-02-08 21:47:49 +0800

    feat: add test settings to pom

--- starter/src/test/java/tw/com/softleader/data/jpa/spec/E2eTest.java
+++ starter/src/test/java/tw/com/softleader/data/jpa/spec/E2eTest.java
@@ -9,24 +9,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import tw.com.softleader.data.jpa.spec.annotation.Spec;
 
 @SpringBootTest
 @AutoConfigureDataJpa
 @SpringBootApplication
+@Transactional
 class E2eTest {
 
   @Autowired
   CustomerRepository repository;
 
   @Test
-  void test() {
+  void simpleCase() {
     var matt = repository.save(Customer.builder().name("matt").build());
     repository.save(Customer.builder().name("bob").build());
 
     var criteria = MyCriteria.builder().hello(matt.getName()).build();
     var actual = repository.findBySpec(criteria);
     assertThat(actual).hasSize(1).contains(matt);
+  }
+
+  @Test
+  void emptyCriteria() {
+    var matt = repository.save(Customer.builder().name("matt").build());
+    var bob = repository.save(Customer.builder().name("bob").build());
+    var mary = repository.save(Customer.builder().name("mary").build());
+
+    var criteria = MyCriteria.builder().build();
+    var actual = repository.findBySpec(criteria);
+    assertThat(actual).hasSize(3).contains(matt, bob, mary);
   }
 
   @Builder
