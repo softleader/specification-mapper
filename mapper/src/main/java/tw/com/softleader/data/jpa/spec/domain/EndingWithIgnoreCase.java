e2dcb8976b719be31f39263302b4b90c93cbfe3a
Kyle Chen <kyle.chen@>
2023-11-24 19:57:42 +0800

    feat: ignore case

---
+++ mapper/src/main/java/tw/com/softleader/data/jpa/spec/domain/EndingWithIgnoreCase.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright © 2022 SoftLeader
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package tw.com.softleader.data.jpa.spec.domain;
+
+import java.util.Objects;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.NonNull;
+
+/**
+ * {@code ... where lower(x.firstname) like %?}
+ *
+ * @author Kyle Chen
+ */
+public class EndingWithIgnoreCase<T> extends SimpleSpecification<T> {
+
+  public EndingWithIgnoreCase(@NonNull Context context, @NonNull String path, @NonNull Object value) {
+    super(context, path, "%" + value);
+  }
+
+  @Override
+  public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+    return builder.like(builder.lower(getPath(root)), Objects.toString(value).toLowerCase());
+  }
+}
---
+++ mapper/src/test/java/tw/com/softleader/data/jpa/spec/domain/StartingWithIgnoreCaseTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2022 SoftLeader
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package tw.com.softleader.data.jpa.spec.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static tw.com.softleader.data.jpa.spec.IntegrationTest.TestApplication.noopContext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tw.com.softleader.data.jpa.spec.IntegrationTest;
+import tw.com.softleader.data.jpa.spec.usecase.Customer;
+import tw.com.softleader.data.jpa.spec.usecase.CustomerRepository;
+
+@IntegrationTest
+class StartingWithIgnoreCaseTest {
+
+  @Autowired
+  CustomerRepository repository;
+
+  @Test
+  void test() {
+    var matt = repository.save(Customer.builder().name("MATT").build());
+    repository.save(Customer.builder().name("BOB").build());
+
+    var spec = new StartingWithIgnoreCase<Customer>(noopContext(), "name", "ma");
+    var actual = repository.findAll(spec);
+    assertThat(actual).hasSize(1).contains(matt);
+  }
+}
